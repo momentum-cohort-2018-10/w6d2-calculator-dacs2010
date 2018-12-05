40342ae9b30b61709b4b4b04256ae8fe00c2a500
xXDuckXx <dacs2010@gmail.com>
2018-12-05 05:56:45 -0500

    equals sign works, display works

--- main.js
+++ main.js
@@ -1,11 +1,52 @@
 
-function click1(event){
-    let thing = document.getElementById("num-1")
-    let display = document.getElementById('display')
-    let newDisplay = document.createElement('h2')
-    thing.onclick = function () {
-        newDisplay.innerText = "1"
-        display.appendChild(newDisplay)
+function equals(event) {
+    let button = document.getElementById("equals")
+    button.onclick = function () {
+        let equation = document.getElementById("display").innerText
+        let sum = eval(equation)
+        document.getElementById("display").innerText = sum
+        console.log("check")
     }
 }
-click1()
+
+function button1(event){
+    let button = document.getElementById("button-1")
+    let text = button.innerText
+    button.onclick = function () {
+        document.getElementById("display").innerText += text
+        console.log("1")
+    }
+}
+function button2(event) {
+    let button = document.getElementById("button-2")
+    let text = button.innerText
+    button.onclick = function () {
+        document.getElementById("display").innerText += text
+    }
+}
+
+function button3(event) {
+    let button = document.getElementById("button-3")
+    let text = button.innerText
+    button.onclick = function () {
+        document.getElementById("display").innerText += text
+    }
+}
+
+function buttonDiv(event) {
+    let button = document.getElementById("button-div")
+    let text = button.innerText
+    button.onclick = function () {
+        document.getElementById("display").innerText += text
+    }
+}
+
+
+button1()
+button2()
+button3()
+buttonDiv()
+
+equals()
+
+console.log("js connected")
